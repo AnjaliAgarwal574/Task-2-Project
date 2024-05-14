38281c391c2b74966079eb800557b8cda56cb4ae
ANJALI AGARWAL <156755739+AnjaliAgarwal574@users.noreply.github.com>
2024-05-14 18:26:27 +0530

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,25 @@
+var button = document.getElementById("redirectButton");
+button.addEventListener("click",function(){
+    var redirectToUrl="https://en.wikipedia.org/wiki/MS_Dhoni";
+    window.location.href=redirectToUrl;
+});
+var button = document.getElementById("inst");
+button.addEventListener("click",function(){
+    var redirectToUrl="https://www.instagram.com/mahi7781/";
+    window.location.href=redirectToUrl;
+});
+var button = document.getElementById("face"); 
+button.addEventListener("click",function(){
+    var redirectToUrl="https://www.facebook.com/MSDhoni/";
+    window.location.href=redirectToUrl;
+});
+var button = document.getElementById("twit");
+button.addEventListener("click",function(){
+    var redirectToUrl="https://twitter.com/msdhoni?lang=en";
+    window.location.href=redirectToUrl;
+});
+var button = document.getElementById("thread");
+button.addEventListener("click",function(){
+    var redirectToUrl="https://www.threads.net/@mahi7781?hl=en";
+    window.location.href=redirectToUrl;
+});
